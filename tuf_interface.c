b14e7b1ec363c834314b427017d0f8bea4ff604e
PoppySeedPlehzr <nanderson7@gmail.com>
2013-11-07 15:36:35 -0500

    Updating tuf_interface.c
    I've currently got the tuf_interface.c function importing the tuf.interposition.configure
    and deconfigure functions, now I'm working on building out the arguments for the functions
    and then they should be callable.

--- tuf_interface.c
+++ tuf_interface.c
@@ -18,36 +18,46 @@
  * Also note, that you *must* have the Python.h include before other standard header
  * includes.  This is per Python 3's documentation regarding how to do the python extensions
  * with c.
+ *
+ * Additionally you should have TUF installed via python.  This is required because
+ * we import the tuf module directly in the code here.
+ *
+ * Best tutorial I've found thus far: http://www.linuxjournal.com/article/8497?page=0,2
  */
 
 #include "python2.7/Python.h"
 
-static PyObject *my_callback = NULL;
-
-static PyObject *my_set_callback(PyObject *dummy, PyObject *args){
-
-    PyObject *result = NULL;
-    PyObject *temp;
-
-    if (PyArg_ParseTuple(args, "0:set_callback", &temp)){
-        if(!PyCallable_Check(temp)){
-            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
-            return NULL;
-        }
-        Py_XINCREF(temp);           /* Add a reference to new callback */
-        Py_XDECREF(my_callback);    /* Dispose of previous callback */
-        my_callback = temp;         /* remember the new callback */
-        /* Boilerplate to retun "None" */
-        Py_INCREF(Py_None);
-        result = Py_None;
-    }
-    return result;
-}
-
-
 
 int main(int argc, char* argv[]){
+
+    Py_Initialize();
+
+    //PyObject *main_module = PyImport_AddModule("__main__");
+    printf("Adding the tuf.interposition module\n");
+    PyObject *tuf_int_mod = PyImport_AddModule("tuf.interposition");
+    printf("Adding the Tuf modules dictionary\n");
+    PyObject *tuf_dict   = PyModule_GetDict(tuf_int_mod); // Get the TUF module's global dictionary
+    printf("Adding the 'configure' function\n");
+    PyObject *conf_func  = PyDict_GetItemString(tuf_dict, "configure"); // Get tuf.interposition.configure method
+    printf("Adding the 'deconfigure' function\n");
+    PyObject *dconf_func = PyDict_GetItemString(tuf_dict, "deconfigure"); // Get tuf.interposition.deconfigure method
+    // TODO: Build the arguments for tuf.interposition
+    //conf_args  = NULL; // Arguments to be passed to tuf.interposition.configure
+    //dconf_args = NULL; 
+
+    // Call the interposition function
+    printf("Calling the configure function with NULL as args\n");
+    PyObject_CallObject(conf_func, NULL);
+    printf("Calling the deconfigure function with NULL as args\n");
+    PyObject_CallObject(dconf_func, NULL);
+    
+    // Decrement the reference counter for each PyObj we've played with
+    Py_DECREF(tuf_int_mod);
+    Py_DECREF(tuf_dict);
+    Py_DECREF(conf_func);
+    Py_DECREF(dconf_func);
     printf("Hello!\n");
+
     return 0;
 }
 
