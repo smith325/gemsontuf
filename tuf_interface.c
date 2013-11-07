95d478ac0469b067bf6b8a8c4dd4148a6d54a14c
PoppySeedPlehzr <nanderson7@gmail.com>
2013-11-07 14:36:27 -0500

    Added example of Python to C interface, updated tuf_interface
    I found an example of calling python functions from within C code
    by specifying the .py file and calling specific functions.  I have
    also started hashing out some of the tuf_interface file, but this was
    following the documentation on the Python site.

--- tuf_interface.c
+++ tuf_interface.c
@@ -1,14 +1,49 @@
 /*
+ * Application Security Fall 2013
+ * Team Red - Project 3
+ *
  * Nick Anderson
- * TUF -> C Interface
+ * Pan Chan
+ * Anthony
+ * Nektarios
+ *
+ * This C code acts as the interface to call TUF functionality
+ * from other languages.  This module should sit between Python, and
+ * whichever destination language one desires.
  *
  * Compiling:
  * gcc tuf_interface.c -o tuf_interface -lpython2.7
  * 
  * You must have python-dev installed for the linker to find the python2.7 libraries.
+ * Also note, that you *must* have the Python.h include before other standard header
+ * includes.  This is per Python 3's documentation regarding how to do the python extensions
+ * with c.
  */
 
 #include "python2.7/Python.h"
+
+static PyObject *my_callback = NULL;
+
+static PyObject *my_set_callback(PyObject *dummy, PyObject *args){
+
+    PyObject *result = NULL;
+    PyObject *temp;
+
+    if (PyArg_ParseTuple(args, "0:set_callback", &temp)){
+        if(!PyCallable_Check(temp)){
+            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+            return NULL;
+        }
+        Py_XINCREF(temp);           /* Add a reference to new callback */
+        Py_XDECREF(my_callback);    /* Dispose of previous callback */
+        my_callback = temp;         /* remember the new callback */
+        /* Boilerplate to retun "None" */
+        Py_INCREF(Py_None);
+        result = Py_None;
+    }
+    return result;
+}
+
 
 
 int main(int argc, char* argv[]){
