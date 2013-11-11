c83299cc53980cd9d419388e2c1014729c0a70bf
PandaSPUR <panhchan@gmail.com>
2013-11-10 20:34:34 -0500

    Moved to ./rubypython

--- scratchspace/rubytopy.rb
+++
@@ -1,9 +0,0 @@
-require "rubygems"
-require "rubypython"
-
-RubyPython.start # start the Python interpreter
-
-httplib = RubyPython.import("urllib")
-print httplib.urlopen("http://google.com")
-
-RubyPython.stop # stop the Python interpreter
