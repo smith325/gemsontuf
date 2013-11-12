e7d74f09e19691b152baa493424107070766078e
Anthony Green <ag3983@nyu.edu>
2013-11-11 20:49:49 -0500

    Typo in header. Gems interface is basically done.

--- GemsOnTuf.c
+++ GemsOnTuf.c
@@ -1,12 +1,18 @@
 /*
- * Usage: 
+ * #Usage: 
  * require 'GemsOnTuf'
- * tuf = GemsOnTuf::TUF.new( args )   #this is equivalent to tuf.interposition.configure()
+ * #this is equivalent to tuf.interposition.configure()
+ * #I also don't think we need the second and third arguments
+ * #unless we're using SSLs
+ * tuf = GemsOnTuf::TUF.new( 'tuf.interposition.json', '', '' )   
  * tuf.method()
- * Methods:
- * deconfigure
- * urlOpen( 'urlString' )
- * etc
+ * #Methods:
+ * tuf.urlOpen( 'urlString' )
+ * tuf.urlOpenTwo( 'urlString' )
+ * tuf.urlRetrieve( 'urlString', 'filename' )
+ * #etc
+ * #The above run if put into a ruby file.
+ * 
  */
 
 #include "ruby.h"
@@ -23,20 +29,23 @@ VALUE method_TUFConfigure( VALUE self, VALUE par0, VALUE par1, VALUE par2 );
 VALUE method_TUFurlOpen( VALUE self, VALUE rbUrl );
 VALUE method_TUFDeconfigure( VALUE self );
 
+//place holders for now
+VALUE method_TUFurlOpenTwo( VALUE self, VALUE rbUrl );
+VALUE method_TUFurlRetrieve( VALUE self, VALUE rbURL, VALUE rbFile );
+
 //init methods ~ require 'GemsOnTuf'
 void Init_GemsOnTuf() {
-	//I'm thinking of initializing the interpreter here so it's the same for all function calls 
-	//Py_Initialize();
-	
-    GemsOnTuf = rb_define_module("GemsOnTuf");
+	GemsOnTuf = rb_define_module("GemsOnTuf");
     TUF = rb_define_class_under( GemsOnTuf, "TUF", rb_cObject );
 	rb_define_method( TUF, "initialize", method_TUFConfigure, 3 );
 	rb_define_method( TUF, "deconfigure", method_TUFDeconfigure, 0 );
 	rb_define_method( TUF, "urlOpen", method_TUFurlOpen, 1 );
+	rb_define_method( TUF, "urlOpenTwo", method_TUFurlOpenTwo, 1 );
+	rb_define_method( TUF, "urlRetrieve", method_TUFurlRetrieve, 2 );
 }
 
 
-//bool Py_TUFConfigure(char* tuf_intrp_json, char* p_repo_dir, char* p_ssl_cert_dir)
+//bool Py_TUF_configure(char* tuf_intrp_json, char* p_repo_dir, char* p_ssl_cert_dir)
 //returns a bool, I think the exception from Python closes the program anyway though
 //so this might not be necessary. 
 VALUE method_TUFConfigure( VALUE self, VALUE par0, VALUE par1, VALUE par2 ) {
@@ -51,10 +60,6 @@ VALUE method_TUFConfigure( VALUE self, VALUE par0, VALUE par1, VALUE par2 ) {
 }
 
 
-/*bool Py_TUF_urllib_urlopen(char* url);
-bool Py_TUF_urllib_urlretreive(char* url, char* fname);
-bool Py_TUF_urllib2_urlopen(char* url) */
-
 //char* Py_TUF_urllib_urlopen( char* url ) 
 VALUE method_TUFurlOpen( VALUE self, VALUE rbUrl ) {
 	char* url = StringValuePtr( rbUrl );
@@ -66,9 +71,34 @@ VALUE method_TUFurlOpen( VALUE self, VALUE rbUrl ) {
 	return rb_str_new2( readUrl );
 }
 
+//bool Py_TUF_urllib2_urlopen(char* url)
+VALUE method_TUFurlOpenTwo( VALUE self, VALUE rbUrl ) {
+	char* url = StringValuePtr( rbUrl );
+	
+	bool worked = Py_TUF_urllib2_urlopen( url );
+	
+	if ( worked ) 
+		return Qtrue;
+	return Qfalse;
+}
+
+
+//bool Py_TUF_urllib_urlretrieve(char* url, char* fname);
+VALUE method_TUFurlRetrieve( VALUE self, VALUE rbUrl, VALUE rbFile ) {
+	char* url = StringValuePtr( rbUrl );
+	char* file = StringValuePtr( rbFile );
+	
+	bool worked = Py_TUF_urllib_urlretrieve( url, file );
+				  
+	if ( worked )
+		return Qtrue;
+	return Qfalse;
+}
+
 //void Py_TUFDeconfigure();
+
 VALUE method_TUFDeconfigure( VALUE self ) {
-	Py_TUFDeconfigure();
+	printf( "I don't know do anything right now.\n" );
 	return Qtrue;
 }
 
--- tuf_interface.h
+++ tuf_interface.h
@@ -1,12 +1,10 @@
 #include "python2.7/Python.h"
-//#include "tuf_interface.c"
 #include <stdbool.h>
 
 bool Py_TUF_configure(char*, char*, char*);
 bool Py_TUF_deconfigure(PyObject*);
-//bool Py_TUF_urllib_urlopen(char*);
 char* Py_TUF_urllib_urlopen(char*);
 bool Py_TUF_urllib2_urlopen(char*);
-bool Py_TUF_urllib_urlretreive(char*, char*);
+bool Py_TUF_urllib_urlretrieve(char*, char*);
 
  
