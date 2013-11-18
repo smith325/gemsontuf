37b772a013c0da019fbadb169c9d21e6f2218f5a
Nektarios Tsoutsos <ngt218@students.poly.edu>
2013-11-18 11:06:09 -0500

    Modified the generation script

--- dev/repoScripts/repoGen.py
+++ dev/repoScripts/repoGen.py
@@ -1,62 +1,123 @@
 from tuf.libtuf import *
 import os
 
+secret_root_1 = "mysecret1"
+secret_root_2 = "mysecret2"
+secret_root_3 = "mysecret3"
+
+secret_targets_1 = "mysecret4"
+secret_targets_2 = "mysecret5"
+
+secret_release_1 = "mysecret6"
+secret_release_2 = "mysecret7"
+
+secret_timestamp_1 = "mysecret8"
+secret_timestamp_2 = "mysecret9"
+
+repoPath = "/home/nek/Desktop/gemsonTUF/gemsontuf/repository/"
+
 #key generate, they're all the same kind of
-prints "Generating keys"
-generate_and_write_rsa_keypair("root_key", bits=2048, password="password")
-generate_and_write_rsa_keypair("targets_key", bits=2048, password="password")
-generate_and_write_rsa_keypair("release_key", bits=2048, password="password")
-generate_and_write_rsa_keypair("timestamp_key", bits=2048, password="password")
+print "Generating keys"
+generate_and_write_rsa_keypair("root_key1",      bits=2048, password=secret_root_1)
+generate_and_write_rsa_keypair("root_key2",      bits=2048, password=secret_root_2)
+generate_and_write_rsa_keypair("root_key3",      bits=2048, password=secret_root_3)
+
+generate_and_write_rsa_keypair("targets_key1",   bits=2048, password=secret_targets_1)
+generate_and_write_rsa_keypair("targets_key2",   bits=2048, password=secret_targets_2)
+
+generate_and_write_rsa_keypair("release_key1",   bits=2048, password=secret_release_1)
+generate_and_write_rsa_keypair("release_key2",   bits=2048, password=secret_release_2)
+
+generate_and_write_rsa_keypair("timestamp_key1", bits=2048, password=secret_timestamp_1)
+generate_and_write_rsa_keypair("timestamp_key2", bits=2048, password=secret_timestamp_2)
 
 #public key import
-prints "Importing keys"
-publicRoot = import_rsa_publickey_from_file("root_key.pub")
-publicTarget = import_rsa_publickey_from_file("targets_key.pub")
-publicRelease = import_rsa_publickey_from_file("release_key.pub")
-publicTime = import_rsa_publickey_from_file("timestamp_key.pub")
+print "Importing keys"
+publicRoot1 = import_rsa_publickey_from_file("root_key1.pub")
+publicRoot2 = import_rsa_publickey_from_file("root_key2.pub")
+publicRoot3 = import_rsa_publickey_from_file("root_key3.pub")
+
+publicTargets1 = import_rsa_publickey_from_file("targets_key1.pub")
+publicTargets2 = import_rsa_publickey_from_file("targets_key2.pub")
+
+publicRelease1 = import_rsa_publickey_from_file("release_key1.pub")
+publicRelease2 = import_rsa_publickey_from_file("release_key2.pub")
+
+publicTimestamp1 = import_rsa_publickey_from_file("timestamp_key1.pub")
+publicTimestamp2 = import_rsa_publickey_from_file("timestamp_key2.pub")
 
 #private key import second parameter is so you don't have to type a password in
-privateRoot = import_rsa_privatekey_from_file("root_key", password="password")
-privateTarget = import_rsa_privatekey_from_file("targets_key", password="password")
-privateRelease = import_rsa_privatekey_from_file("release_key", password="password")
-privateTime = import_rsa_privatekey_from_file("timestamp_key", password="password")
+privateRoot1 = import_rsa_privatekey_from_file("root_key1", password=secret_root_1)
+privateRoot2 = import_rsa_privatekey_from_file("root_key2", password=secret_root_2)
+privateRoot3 = import_rsa_privatekey_from_file("root_key3", password=secret_root_3)
+
+privateTargets1 = import_rsa_privatekey_from_file("targets_key1", password=secret_targets_1)
+privateTargets2 = import_rsa_privatekey_from_file("targets_key2", password=secret_targets_2)
+
+privateRelease1 = import_rsa_privatekey_from_file("release_key1", password=secret_release_1)
+privateRelease2 = import_rsa_privatekey_from_file("release_key2", password=secret_release_2)
+
+privateTimestamp1 = import_rsa_privatekey_from_file("timestamp_key1", password=secret_timestamp_1)
+privateTimestamp2 = import_rsa_privatekey_from_file("timestamp_key2", password=secret_timestamp_2)
 
 #create new repository directory
-prints "Building repository"
-repository = create_new_repository("repository")
+print "Building repository"
+repository = create_new_repository(repoPath)
 
 #adds public keys to directory
-repository.root.add_key(publicRoot)
-repository.targets.add_key(publicTarget)
-repository.release.add_key(publicRelease)
-repository.timestamp.add_key(publicTime)
+repository.root.add_key(publicRoot1)
+repository.root.add_key(publicRoot2)
+repository.root.add_key(publicRoot3)
+
+repository.targets.add_key(publicTargets1)
+repository.targets.add_key(publicTargets2)
+
+repository.release.add_key(publicRelease1)
+repository.release.add_key(publicRelease2)
+
+repository.timestamp.add_key(publicTimestamp1)
+repository.timestamp.add_key(publicTimestamp2)
+
+#create thresholds
+repository.root.threshold = 3
+repository.targets.threshold = 2
+repository.release.threshold = 2
+repository.timestamp.threshold = 2
+
+#load singing keys
+repository.root.load_signing_key(privateRoot1)
+repository.root.load_signing_key(privateRoot2)
+repository.root.load_signing_key(privateRoot3)
+
+repository.targets.load_signing_key(privateTargets1)
+repository.targets.load_signing_key(privateTargets2)
+
+repository.release.load_signing_key(privateRelease1)
+repository.release.load_signing_key(privateRelease2)
+
+repository.timestamp.load_signing_key(privateTimestamp1)
+repository.timestamp.load_signing_key(privateTimestamp2)
 
 #expiration date
-repository.timestamp.expiration = "2014-10-28 12:08:00"
+repository.timestamp.expiration = "2014-10-10 12:00:00"
 
-#creates a symlink to the repository and then creates the target metadata 
-#adds targets metadata
-#use an asbolute path apparently Ubuntu doesn't like this so if you get an error like
-#not a file ../test/test/test/test/etc this is why
+#set compression mode
+repository.targets.compressions = ["gz"]
+repository.release.compressions = ["gz"]
+
+#Add new targets
 print "Building targets file"
-os.symlink("../../repository/targets", "./repository/targets/test")
-targetFiles = repository.get_filepaths_in_directory("./repository/targets/test",
-                                                        recursive_walk=True, followlinks=True)
+targetFiles = repository.get_filepaths_in_directory(repoPath + "targets/", recursive_walk=True, followlinks=True)
 repository.targets.add_targets(targetFiles)
-
-#creates threshold for root and loads private keys
-repository.root.threshold = 1
-repository.root.load_signing_key(privateRoot)
-repository.targets.load_signing_key(privateTarget)
-repository.release.load_signing_key(privateRelease)
-repository.timestamp.load_signing_key(privateTime)
 
 #prints some information about the repository setup
 repository.status()
 
 #tries to create repository 
-print "Writing repository directory"
+print "Writing repository-stage"
 try:
   repository.write()
 except tuf.Error, e:
   print e 
+
+
